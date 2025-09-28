dd38deb33dadc620a28759dcc3038cdb12d48b00
Habababanegra <tomicd99@gmail.com>
2025-09-29 00:16:39 +0200

    Punto

--- scripts/punto.gd
+++ scripts/punto.gd
@@ -1,25 +1,91 @@
 extends Node2D
-func _draw():
-	draw_circle(Vector2(0,0),10.0,Color.RED)
+
+var radio_rojo = 5 #Zona peligro maximo
+var radio_naranja = 15 #Zona peligro
+var radio_amarillo = 25 #Donde se rompen cristales
+	
 var lon = 133.775136 * 2*PI / 360
-var lat = -25.274398 * 2*PI / 360
+var lat = 80 * 2*PI / 360
 var a = 0
 var b = 0
 var scale_factor = 1.635
-func _process(delta):
-	z_index = 1
+
+
+@onready var camera_2d: Camera2D = $"../Camera2D"
+
+
+
+func lat_lon_conversion(lon,lat):
+	
 	a = get_parent().get_node('MapaMundo').get_rect().size.x
 	b = get_parent().get_node('MapaMundo').get_rect().size.y
 	
-	var x = a * lon/(2*PI)
-	var y = -log(tan(PI/4 + lat/2))*b/(2*PI)
-	position = Vector2(x,y)
+	return Vector2(a * lon/(2*PI),-log(tan(PI/4 + lat/2))*b/(2*PI))
 	
+func lat_lon_conversion_inversa(x,y):
+	a = get_parent().get_node('MapaMundo').get_rect().size.x
+	b = get_parent().get_node('MapaMundo').get_rect().size.y
+	
+	return Vector2(2*PI*x/a,2*atan(exp(-2*PI*y/b))-PI/2)
+
+func _draw():
+	
+	
+	
+	var conversion_elipse = (40074 * get_parent().get_node('MapaMundo').get_rect().size.x) / (get_parent().get_node('MapaMundo').get_rect().size.y * 2*PI*6378) 
+	
+	draw_set_transform(Vector2(0,0),0,Vector2(conversion_elipse*10,10))
+	
+	draw_circle(Vector2(0,0),radio_amarillo,Color(Color.ORANGE,0.45))
+	draw_circle(Vector2(0,0),radio_naranja,Color(Color.ORANGE_RED,0.5))
+	draw_circle(Vector2(0,0),radio_rojo,Color(Color.RED,0.55))
+
+func _ready():
+	
+	z_index = 1
+	
+	position = lat_lon_conversion(lon,lat)
+
+	add_child(parallax)
+	layer.motion_scale = Vector2(0, 0)  # no se mueve ni con la cámara ni con zoom
+	parallax.add_child(layer)
+	layer.add_child(rich_text_label)
+	
+	rich_text_label.z_index = 1
+	
+	
+var parallax = ParallaxBackground.new()
+
+var layer = ParallaxLayer.new()
+
+func _process(delta):
+	
+	pass
+	
+@onready var rich_text_label: RichTextLabel = $"../CanvasLayer/RichTextLabel"
+
+	
+
 func _input(event):
 	# Mouse in viewport coordinates.
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT):
-		print("Mouse Click/Unclick at: ", event.position)
-	
+
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		position = camera_2d.get_global_mouse_position()
+		
+		rich_text_label.scale = Vector2(2,2)
+		
+		if terreno:
+			rich_text_label.text = '[outline_size={5}]Has tocado tierra[/outline_size]'
+		else:
+			rich_text_label.text = '[outline_size={5}]Has tocado mar[/outline_size]'
 	#if event.is_action_pressed("keyQ"):
 		#scale_factor += 0.01
 		#print(scale_factor)
+
+var terreno = true
+
+func _on_area_2d_mouse_entered() -> void:
+	terreno = true
+	
+func _on_area_2d_mouse_exited() -> void:
+	terreno = false
